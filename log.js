198ea8efb39987c4ae1f5c86126408fe8a2f0678
ashleybarkworth <ashley.barkworth@gmail.com>
2021-05-23 15:10:32 -0700

    fixed keyboard event handling + changed timestamp

--- log.js
+++ log.js
@@ -2,31 +2,42 @@ $(function () {
     var textFile = null;
     mouseData = "";
     keyData = "";
+    // A map to remember keys that are held down
+    var keysdown = {};
 
     $(document).mousemove(function(e) {
-        mouseData += $.now() + ",";
+        mouseData += e.timeStamp + ",";
         mouseData += e.pageX + ",";
         mouseData += e.pageY + ",";
         mouseData += "mousemove\n";
     });
 
     $(document).click(function(e) {
-        mouseData += $.now() + ",";
+        mouseData += e.timeStamp + ",";
         mouseData += e.pageX + ",";
         mouseData += e.pageY + ",";
         mouseData += "mouseclick\n";
     });
 
     $("#textStuff").keydown(function(e) {
-        keyData += $.now() + ",";
-        keyData += e.keyCode + ",";
+        if (keysdown[e.keyCode]) {
+          // Ignore it
+          return;
+        }
+
+        // Remember it's down
+        keysdown[e.keyCode] = true;
+
+        keyData += e.timeStamp + ",";
+        keyData += e.key + ",";
         keyData += "keydown\n";
     });
 
     $("#textStuff").keyup(function(e) {
-        keyData += $.now() + ",";
-        keyData += e.keyCode + ",";
+        keyData += e.timeStamp + ",";
+        keyData += e.key + ",";
         keyData += "keyup\n";
+        delete keysdown[e.keyCode];
     });
 
 });
